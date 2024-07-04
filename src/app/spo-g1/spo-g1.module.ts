46ef40c99a16ef92cbc0103c4f6e7923a6e399a6
nour ben aissa <nour.benaissa@esprit.tn>
2024-07-04 15:56:18 +0100

    Module creation

---
+++ src/app/spo-g1/spo-g1.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpoG1RoutingModule } from './spo-g1-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpoG1RoutingModule
+  ]
+})
+export class SpoG1Module { }
